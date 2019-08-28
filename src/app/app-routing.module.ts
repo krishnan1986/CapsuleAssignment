48255560a1823b7dfb0d63edf2f0f051e38579bc
krishnan <krishnan@cognizant.com>
2019-08-28 15:19:28 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'AddTask',component: AddTaskComponent},
+{path:'ViewTask',component: ViewTaskComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
+import { TaskHomeComponent } from './task-home/task-home.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import {AlertModule} from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewTaskComponent,
+    UpdateTaskComponent,
+    TaskHomeComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
